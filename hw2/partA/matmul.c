f7d0706e4ca1a8985c4e516f1a88a2554f5adbcc
Gaurangkarwande <gkarwande18@gmail.com>
2022-02-22 01:48:37 -0500

    sections done

--- hw2/partA/matmul.c
+++ hw2/partA/matmul.c
@@ -6,23 +6,32 @@
 #define BLOCK_SIZE 32
 #define min(a,b) (((a)<(b))?(a):(b))
 
-void initialize_matrix(int row, int col, double mat[], int is_result)
+void initialize_matrix(int row, int col, int mat[])
 {
-    srand(0);
     int i, j;
     for ( i = 0; i < row; i++) {
         for (j = 0; j < col; j++) {
-            if (is_result) {
-                mat[i*col + j] = 0;
-            } else {
-                mat[i*col + j] = rand() % (row*col);
-            } 
+            mat[i*col + j] = 0;
+            if (i == j)
+                mat[i*col + j] = 1;
         }
     }
    return; 
 }
 
-int compare_matrix(int r1, int c1, double first[], int r2, int c2, double second[])
+void print_matrix(int row, int col, int mat[])
+{
+    int i, j;
+    for ( i = 0; i < row; i++) {
+        for (j = 0; j < col; j++) {
+            printf("%d \t", mat[i*col + j]);
+        }
+        printf("\n");
+    }
+   return; 
+}
+
+int compare_matrix(int r1, int c1, int first[], int r2, int c2, int second[])
 {
     for (int i = 0; i < r1; ++i)
         for (int j = 0; j < c2; ++j) 
@@ -31,33 +40,34 @@ int compare_matrix(int r1, int c1, double first[], int r2, int c2, double second
     return 0;
 }
 
-void multiply_serial_naive(int r1, int c1, double first[], int r2, int c2, double second[], double result[])
+void multiply_naive(int first[], int second[], int result[], int dim)
 {
-    int i,j,k;
-    double first_ik;
-    for (i = 0; i < r1; i++) {
-        for (k = 0; k < c1; k++) {
-            first_ik = first[i*c1 + k];
-            for (j = 0; j < c2; j++) {
-                result[i*c2 + j] += first_ik * second[k*c2 + j];
+    int i,j,k, sum;
+    for (i = 0; i < dim; i++) {
+        for (j = 0; j < dim; j++) {
+            sum = 0;
+            for (k = 0; k < dim; k++) {
+                sum += first[i*dim + k] * second[k*dim + j];
             }
+            result[i*dim + j] = sum;
         }
     }
 }
 
-void multiply_serial_block(int r1, int c1, double first[], int r2, int c2, double second[], double result[])
+void multiply_serial_block(int first[], int second[], int result[], int dim)
 {
-    int ii, jj, kk, i, j, k, first_ik, blk_size = BLOCK_SIZE;
+    int ii, jj, kk, i, j, k,sum, blk_size = BLOCK_SIZE;
 
-	for(ii = 0; ii < r1; ii += blk_size) {
-        for(kk = 0; kk < c1; kk += blk_size) {
-            for(jj = 0; jj < c2; jj += blk_size) {
-				for(i = ii; i < min(r1, ii+blk_size); i++) {
-                    for(k = kk; k < min(c1, kk+blk_size); k++) {
-                        first_ik = first[i*c1 + k];
-                        for(j = jj; j < min(c2, jj+blk_size); j++) {
-							result[i*c2 + j] += first_ik * second[k*c2 + j]; 
+	for(ii = 0; ii < dim; ii += blk_size) {
+        for(jj = 0; jj < dim; jj += blk_size) {
+            for(kk = 0; kk < dim; kk += blk_size) {
+				for(i = ii; i < min(dim, ii+blk_size); i++) {
+                    for(j = jj; j < min(dim, jj+blk_size); j++) {
+                        sum = 0;
+                        for(k = kk; k < min(dim, kk+blk_size); k++) {
+							sum += first[i*dim + k] * second[k*dim + j]; 
                         }
+                        sum = result[i*dim + j];
                     }
                 }
             }
@@ -65,89 +75,211 @@ void multiply_serial_block(int r1, int c1, double first[], int r2, int c2, doubl
     }											
 }
 
-void multiply_omp(int r1, int c1, double first[], int r2, int c2, double second[], double result[])
+void multiply_omp(int first[], int second[], int result[], int dim)
 {
-    int i,j,k;
-    double first_ik;
-    #pragma omp parallel for collapse(2) private(i,j,k) shared(first, second, result, r1, r2, c1, c2) num_threads(64)
-        for (i = 0; i < r1; i++) {
-            for (k = 0; k < c1; k++) {
-                first_ik = first[i*c1 + k];
-                for (j = 0; j < c2; j++) {
-                    result[i*c2 + j] += first_ik * second[k*c2 + j];
+    int i,j,k, sum;
+    #pragma omp parallel for collapse(2) private(i,j,k, sum) shared(first, second, result)
+        for (i = 0; i < dim; i++) {
+            for (j = 0; j < dim; j++) {
+                sum = 0;
+                for (k = 0; k < dim; k++) {
+                    sum += first[i*dim + k] * second[k*dim + j];
                 }
+                result[i*dim + j] = sum;
             }
-        }
+    }
 }
 
-void multiply_omp_block(int r1, int c1, double first[], int r2, int c2, double second[], double result[])
+void multiply_omp_block(int first[], int second[], int result[], int dim)
 {
-    int ii, jj, kk, i, j, k, first_ik, blk_size = BLOCK_SIZE;
+    int ii, jj, kk, i, j, k,sum, blk_size = BLOCK_SIZE;
 
-    #pragma omp parallel for private(i,j,k, ii, jj, kk) shared(first, second, result, r1, r2, c1, c2, blk_size) num_threads(32)
-	for(ii = 0; ii < r1; ii += blk_size) {
-        for(kk = 0; kk < c1; kk += blk_size) {
-            for(jj = 0; jj < c2; jj += blk_size) {
-				for(i = ii; i < min(r1, ii+blk_size); i++) {
-                    for(k = kk; k < min(c1, kk+blk_size); k++) {
-                        first_ik = first[i*c1 + k];
-                        for(j = jj; j < min(c2, jj+blk_size); j++) {
-							result[i*c2 + j] += first_ik * second[k*c2 + j]; 
+    #pragma omp parallel for collapse(3) private(i,j,k, ii, jj, kk, sum) shared(first, second, result, blk_size)
+        for(ii = 0; ii < dim; ii += blk_size) {
+            for(jj = 0; jj < dim; jj += blk_size) {
+                for(kk = 0; kk < dim; kk += blk_size) {
+                    for(i = ii; i < min(dim, ii+blk_size); i++) {
+                        for(j = jj; j < min(dim, jj+blk_size); j++) {
+                            sum = 0;
+                            for(k = kk; k < min(dim, kk+blk_size); k++) {
+                                sum += first[i*dim + k] * second[k*dim + j]; 
+                            }
+                            sum = result[i*dim + j];
                         }
                     }
                 }
             }
-        }
-    }											
+        }											
 }
 
-void multiply_omp_sections(int r1, int c1, double first[], int r2, int c2, double second[], double result[])
+void multiply_omp_sections(int first[], int second[], int result[], int dim)
 {
-    int i,j,k;
-    double first_ik = 0.0;
-        for (i = 0; i < r1; i++) {
-            #pragma omp parallel sections shared(first, second, result, r1, r2, c1, c2)
-            {
-            #pragma omp section
-                for (k = 0; k < c1; k++) {
-                    first_ik = first[i*c1 + k];
-                    for (j = 0; j < c2; j++) {
-                        result[i*c2 + j] += first_ik * second[k*c2 + j];
+    int num_sections = 8;
+    int section_size = dim/num_sections;
+    #pragma omp parallel sections shared(first, second, result)
+    {
+        #pragma omp section
+        {
+            int i,j,k, sum;
+            for (i = 0; i < section_size; i++) {
+                for (j = 0; j < dim; j++) {
+                    sum = 0;
+                    for (k = 0; k < dim; k++) {
+                        sum += first[i*dim + k] * second[k*dim + j];
                     }
+                    result[i*dim + j] = sum;
                 }
             }
         }
+
+        #pragma omp section
+        {
+            int i,j,k, sum;
+            for (i = section_size; i < section_size*2; i++) {
+                for (j = 0; j < dim; j++) {
+                    sum = 0;
+                    for (k = 0; k < dim; k++) {
+                        sum += first[i*dim + k] * second[k*dim + j];
+                    }
+                    result[i*dim + j] = sum;
+                }
+            }
+        }
+
+        #pragma omp section
+        {
+            int i,j,k, sum;
+            for (i = section_size*2; i < section_size*3; i++) {
+                for (j = 0; j < dim; j++) {
+                    sum = 0;
+                    for (k = 0; k < dim; k++) {
+                        sum += first[i*dim + k] * second[k*dim + j];
+                    }
+                    result[i*dim + j] = sum;
+                }
+            }
+        }
+
+        #pragma omp section
+        {
+            int i,j,k, sum;
+            for (i = section_size*3; i < section_size*4; i++) {
+                for (j = 0; j < dim; j++) {
+                    sum = 0;
+                    for (k = 0; k < dim; k++) {
+                        sum += first[i*dim + k] * second[k*dim + j];
+                    }
+                    result[i*dim + j] = sum;
+                }
+            }
+        }
+
+        #pragma omp section
+        {
+            int i,j,k, sum;
+            for (i = section_size*4; i < section_size*5; i++) {
+                for (j = 0; j < dim; j++) {
+                    sum = 0;
+                    for (k = 0; k < dim; k++) {
+                        sum += first[i*dim + k] * second[k*dim + j];
+                    }
+                    result[i*dim + j] = sum;
+                }
+            }
+        }
+
+        #pragma omp section
+        {
+            int i,j,k, sum;
+            for (i = section_size*5; i < section_size*6; i++) {
+                for (j = 0; j < dim; j++) {
+                    sum = 0;
+                    for (k = 0; k < dim; k++) {
+                        sum += first[i*dim + k] * second[k*dim + j];
+                    }
+                    result[i*dim + j] = sum;
+                }
+            }
+        }
+
+        #pragma omp section
+        {
+            int i,j,k, sum;
+            for (i = section_size*6; i < section_size*7; i++) {
+                for (j = 0; j < dim; j++) {
+                    sum = 0;
+                    for (k = 0; k < dim; k++) {
+                        sum += first[i*dim + k] * second[k*dim + j];
+                    }
+                    result[i*dim + j] = sum;
+                }
+            }
+        }
+
+        #pragma omp section
+        {
+            int i,j,k, sum;
+            for (i = section_size*7; i < section_size*8; i++) {
+                for (j = 0; j < dim; j++) {
+                    sum = 0;
+                    for (k = 0; k < dim; k++) {
+                        sum += first[i*dim + k] * second[k*dim + j];
+                    }
+                    result[i*dim + j] = sum;
+                }
+            }
+        }
+
+    }
 }
 
 int main()
 {
     int dim = 1024;
-    double start, time_s, time_p;
-    double* A = malloc((dim * dim) * sizeof(double));
-    double* B = malloc((dim * dim) * sizeof(double));
-    double* C = malloc((dim * dim) * sizeof(double));
-    double* Baseline = malloc((dim * dim) * sizeof(double));
+    double start, time_s, time_p, time_sec;
+    int* A = malloc((dim * dim) * sizeof(int));
+    int* B = malloc((dim * dim) * sizeof(int));
+    int* C = malloc((dim * dim) * sizeof(int));
+    int* Baseline = malloc((dim * dim) * sizeof(int));
 
-    initialize_matrix(dim, dim, A, 0);
-    initialize_matrix(dim, dim, B, 0);
-    initialize_matrix(dim, dim, C, 1);
-    initialize_matrix(dim, dim, Baseline, 1);
+    initialize_matrix(dim, dim, A);
+    initialize_matrix(dim, dim, B);
+    initialize_matrix(dim, dim, C);
+    initialize_matrix(dim, dim, Baseline);
+
+    omp_set_dynamic(0);
+    omp_set_num_threads(NUM_THREADS);
 
     start = omp_get_wtime();
-    multiply_serial_naive(dim, dim, A, dim, dim, B, Baseline);
+    multiply_naive(A, B, Baseline, dim);
     time_s = omp_get_wtime() - start;
 
     start = omp_get_wtime();
-    multiply_omp_sections(dim, dim, A, dim, dim, B, C);
+    multiply_omp_block(A, B, C, dim);
     time_p = omp_get_wtime() - start;
+
+    start = omp_get_wtime();
+    multiply_omp_sections(A, B, C, dim);
+    time_sec = omp_get_wtime() - start;
 
 
     if (compare_matrix(dim, dim, Baseline, dim, dim, C) == -1)
     {
         printf("Matrix multiplication is wrong\n");
+        if (dim <= 16)
+        {
+            printf("Matrix A: \n");
+            print_matrix(dim, dim, A);
+            printf("Matrix B: \n");
+            print_matrix(dim, dim, B);
+            printf("Matrix Baseline: \n");
+            print_matrix(dim, dim, Baseline);
+            printf("Matrix C: \n");
+            print_matrix(dim, dim, C);
+        }
         return -1;
     }
 
-    printf("Time for serial: %f ms \t Time for parallel: %f ms \n", time_s*1000, time_p*1000);
+    printf("Time for serial: %f ms \t Time for loop parallel: %f ms \t Time for section parallel: %f ms \n", time_s*1000, time_p*1000, time_sec*1000);
     return 0;
 }

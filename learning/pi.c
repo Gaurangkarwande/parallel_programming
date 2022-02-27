f92cea89d7e2c98d1d4cbfce5c2429983f559c95
Gaurangkarwande <gkarwande18@gmail.com>
2022-02-26 20:48:11 -0500

    implement quicksort

--- learning/pi.c
+++ learning/pi.c
@@ -53,11 +53,26 @@ void calculate_pi_critical(double *pi)
     }
 }
 
+void calculate_pi_for(double *pi)
+{
+    int i;
+    double x, sum = 0.0;
+    step = 1.0/(double) num_steps;
+    omp_set_num_threads(NUM_THREADS);
+    #pragma omp parallel for reduction(+:sum) 
+        for (i = 0; i < num_steps; i++)
+        {
+            x = (i+0.5)*step;
+            sum += 4.0/(1.0 + x*x);
+        }
+    *pi = step*sum;
+}
+
 int main()
 {
     double time, start, end, pi = 0.0;
     start = omp_get_wtime();
-    calculate_pi_critical(&pi);
+    calculate_pi_for(&pi);
     end = omp_get_wtime();
     time = end - start;
     printf("Num Threads: %d: pi is %f and Time elapsed: %f ms \n",NUM_THREADS, pi, time);
---
+++ learning/quicksort.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#define NUM_THREADS 2
+
+int level = 0;
+
+void initialize_array(int dim, int arr[])
+{
+    srand(345);
+    int i;
+    for ( i = 0; i < dim; i++) {
+        arr[i] = rand() % dim;
+    }
+   return; 
+}
+
+void print_array(int dim, int arr[])
+{
+    int i;
+    printf("Array is: \n");
+    for ( i = 0; i < dim; i++) {
+        printf("%d, ", arr[i]);
+    }
+    printf("\n");
+   return; 
+}
+
+void swap(int *a, int *b)
+{
+   int temp = *a;
+   *a = *b;
+   *b = temp;
+} 
+
+int partition(int low, int high, int arr[])
+{
+    int i, j, pivot;
+    pivot = arr[high]; i = low - 1;
+    for (j = low; j <= high; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i+1], &arr[high]);
+    return i + 1;
+}
+
+int partition_r(int low, int high, int arr[])
+{
+    srand(213);
+    int random = low + rand() % (high - low);
+    swap(&arr[random], &arr[high]);
+    return partition(low, high, arr);
+}
+
+void quicksort_ser(int low, int high, int arr[])
+{
+    if (low < high)
+    {
+        int pivot = partition_r(low, high, arr);
+        quicksort_ser(low, pivot-1, arr);
+        quicksort_ser(pivot+1, high, arr);
+    }
+}
+
+void quicksort_par(int low, int high, int arr[])
+{
+    if (low < high)
+    {
+        int pivot = partition(low, high, arr);
+        #pragma omp parallel
+        {
+            #pragma omp single
+            {
+                #pragma omp task
+                {
+                    quicksort_par(low, pivot-1, arr);//Thread 1
+                }
+                #pragma omp task
+                {
+                    quicksort_par(pivot+1, high, arr);//Thread2
+                }
+            }
+        }
+    }
+}
+
+// void quicksort(int low, int high, int arr[])
+// {
+//     printf("%d\n", level++);
+//     if (low < high)
+//     {
+//         int pivot = partition_r(low, high, arr);
+//         #pragma omp single
+//         {
+//             #pragma omp task
+//                 quicksort(low, pivot-1, arr);
+//             #pragma omp task
+//                 quicksort(pivot+1, high, arr);
+//         }
+//     }
+// }
+
+// void sort(int dim, int arr[])
+// {
+//     #pragma omp parallel shared(dim, arr)
+//     {
+//         quicksort(0, dim-1, arr);
+//     }
+// }
+
+int main()
+{
+    int dim = 0.5e3;
+    double start, time_s, time_p;
+    int* A = malloc(dim * sizeof(int));
+    int* B = malloc(dim * sizeof(int));
+    initialize_array(dim, A);
+    initialize_array(dim, B);
+
+    omp_set_num_threads(NUM_THREADS);
+    start = omp_get_wtime();
+    quicksort_ser(0, dim-1, B);
+    time_s = omp_get_wtime() - start;
+
+    start = omp_get_wtime();
+    quicksort_par(dim, A);
+    time_p = omp_get_wtime() - start;
+
+    printf("time for serial: %f, time for %d threads: %f\n", time_s, NUM_THREADS, time_p);
+    
+    return 0;
+}

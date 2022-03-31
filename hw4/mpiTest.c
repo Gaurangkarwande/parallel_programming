15b6f4427fb2e8ca5e64dfc0ca885218b60400bb
Gaurangkarwande <gkarwande18@gmail.com>
2022-03-30 23:25:55 -0400

    starting q4

--- hw4/mpiTest.c
+++ hw4/mpiTest.c
@@ -5,30 +5,28 @@
 
 int main(int argc, char *argv[])
 {
-   int rank, numtasks, dest, source, count, tag=1;
-   char inmsg, outmsg='x';
-   MPI_Status Stat;
+   // Get the rank and size in the original communicator
+   int world_rank, world_size;
    MPI_Init(&argc,&argv);
-   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-   MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-   if (rank==0)
-   {
-      dest=1; source=1;
-      for(dest=1, source=1; dest < numtasks && source<numtasks; source++, dest++)
-      {
-         MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
-         MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
-      }
-   }
-   else
-   {
-      dest=0; source=0;
-      MPI_Recv(&inmsg, 1, MPI_CHAR, source, tag, MPI_COMM_WORLD, &Stat);
-      MPI_Send(&outmsg, 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
-   }
-   MPI_Get_count(&Stat, MPI_CHAR, &count);
-   printf("Task %d: Received %d char(s) from task %d with tag %d \n", 
-            rank, count, Stat.MPI_SOURCE, Stat.MPI_TAG);
+   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+   int color = world_rank % 2; // Determine color based on row
+
+   // Split the communicator based on the color and use the
+   // original rank for ordering
+   MPI_Comm row_comm;
+   MPI_Comm_split(MPI_COMM_WORLD, color, world_rank, &row_comm);
+
+   int row_rank, row_size;
+   MPI_Comm_rank(row_comm, &row_rank);
+   MPI_Comm_size(row_comm, &row_size);
+
+   printf("WORLD RANK/SIZE: %d/%d \t ROW RANK/SIZE: %d/%d\n",
+      world_rank, world_size, row_rank, row_size);
+
+   MPI_Comm_free(&row_comm);
+
    MPI_Finalize();
    return 0;
 }
